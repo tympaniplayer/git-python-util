5ead2d57028b0796c90f7f235c30ab9004b1e47e
Nate Palmer <np@configit.com>
2015-05-06 13:11:27 -0400

    Don't import me bro

--- pythonutil.py
+++ pythonutil.py
@@ -3,24 +3,25 @@ from sys import argv, exit
 from git import Repo, InvalidGitRepositoryError, NoSuchPathError
 join = os.path.join
 
-
-if len(argv) < 2:
-	print "Please specify a github repo"
-	exit(1)
-
-repoPath = argv[1];
-
-try:
-	repo = Repo(repoPath)
-except InvalidGitRepositoryError:
-	print "Path is not a git repository"
-	exit(1)
-except  NoSuchPathError:
-	print "Path does not exist"
-	exit(1)
-
-for untracked in repo.untracked_files:
-	print untracked
+def main():
+	if len(argv) < 2:
+		print "Please specify a github repo"
+		exit(1)
 	
-
+	repoPath = argv[1];
+	
+	try:
+		repo = Repo(repoPath)
+	except InvalidGitRepositoryError:
+		print "Path is not a git repository"
+		exit(1)
+	except  NoSuchPathError:
+		print "Path does not exist"
+		exit(1)
+	
+	for untracked in repo.untracked_files:
+		print untracked
+	
+if __name__ == '__main__':
+	main()
 	
