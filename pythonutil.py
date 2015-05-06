1c56b9744c833f247d12f005d12c601d1b9a26a8
Nate Palmer <np@configit.com>
2015-05-06 12:30:46 -0400

    Adding initial script

---
+++ pythonutil.py
@@ -0,0 +1,26 @@
+import os
+from sys import argv, exit
+from git import Repo, InvalidGitRepositoryError, NoSuchPathError
+join = os.path.join
+
+
+if len(argv) < 2:
+	print "Please specify a github repo"
+	exit(1)
+
+repoPath = argv[1];
+
+try:
+	repo = Repo(repoPath)
+except InvalidGitRepositoryError:
+	print "Path is not a git repository"
+	exit(1)
+except  NoSuchPathError:
+	print "Path does not exist"
+	exit(1)
+
+for untracked in repo.untracked_files:
+	print untracked
+	
+
+	
